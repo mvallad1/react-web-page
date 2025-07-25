8bfd01ebe50adcc42519ec620e88ff970ae22843
Moses Valladares <mvallad1@binghamton.edu>
2025-07-25 19:35:13 -0400

    added styling for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Nav from './Navigation/Nav.jsx'
 import Products from './Products/Products.jsx';
 import Recommended from './Recommended/Recommended.jsx';
+import './index.css'
 
 
 function App() {
