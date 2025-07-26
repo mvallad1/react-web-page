88fc882a5af1f953a5e56600fe40c870cf6a3363
Moses Valladares <mvallad1@binghamton.edu>
2025-07-25 23:50:49 -0400

    added category selector with styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Nav from './Navigation/Nav.jsx'
 import Products from './Products/Products.jsx';
 import Recommended from './Recommended/Recommended.jsx';
 import './index.css'
+import Sidebar from './Sidebar/Sidebar.jsx';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return <>
+    <Sidebar />
     <Nav />
-    <Products />
     <Recommended />
+    <Products />
 
   </>;
 }
